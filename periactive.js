127d799f7a8ec20efad9bba711aa412448c97305
goeru-code <goran.eres@gmail.com>
2024-12-08 19:40:27 +0100

    Add files via upload

--- periactive.js
+++ periactive.js
@@ -1,7 +1,7 @@
        function showGas() {
            // elements gas at roomtemp.
-           const myElement = document.getElementById('H');
-           myElement.style = "default";
+           const divElement = document.getElementById("H");
+           divElement.style = "default-div";
            
            const button = document.getElementById("gas");
            button.style.backgroundColor = "black";
@@ -15,10 +15,10 @@
 
        function showLiquid() {
            // elements liquid at roomtemp.
-           const myElement = document.getElementById('H');
+          const myElement = document.getElementById('H');
            myElement.style.color = "gray";
            myElement.style.backgroundColor = "lightgray";
-           myElement.style.border = "0px";
+           myElement.style.border = "0px";  
 
            const button4 = document.getElementById("gas");
            button4.style.backgroundColor = "lightgray";
@@ -36,16 +36,12 @@
            myElement.style.color = "gray";
            myElement.style.backgroundColor = "lightgray";
            myElement.style.border = "0px";
-           //***********************************************************
            const button12 = document.getElementById("gas");
            button12.style.backgroundColor = "lightgray";
-
            const button13 = document.getElementById("liquid");
            button13.style.backgroundColor = "lightgray";
-
            const button14 = document.getElementById("all");
            button14.style.backgroundColor = "lightgray";
-
            const button15 = document.getElementById("solid");
            button15.style.backgroundColor = "black";
        }
@@ -53,20 +49,16 @@
        function showAll() {
            const button8 = document.getElementById("gas");
            button8.style.backgroundColor = "lightgray";
-
            const button9 = document.getElementById("liquid");
            button9.style.backgroundColor = "lightgray";
-
            const button10 = document.getElementById("all");
            button10.style.backgroundColor = "black";
-
            const button11 = document.getElementById("solid");
            button11.style.backgroundColor = "lightgrey";
 
            const gridContainer = document.querySelector('.grid-container');
            const divs = gridContainer.querySelectorAll('div');
            divs.forEach(div => {
-               div.style = "default";
+            div.style = "default";
            });
-
        }
