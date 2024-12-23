8ef2c7fc533042e5bb3bfbabc3db850cd065b06f
goeru-code <goran.eres@gmail.com>
2024-12-23 23:06:24 +0100

    periactiveTable v0.2 supports temperature slider now.

--- periactive.js
+++ periactive.js
@@ -23,7 +23,19 @@
                 div.style.color = "gray";
              }
            });
+           const element = document.getElementById("tempSlider");
+  element.style.visibility = "hidden";
+  const element2 = document.getElementById("temperature");
+  element2.style.visibility = "hidden";
+  const element3 = document.getElementById("buttonsolid");
+  element3.style.visibility = "hidden";
+  const element4 = document.getElementById("buttonliquid");
+  element4.style.visibility = "hidden";
+  const element5 = document.getElementById("buttongas");
+  element5.style.visibility = "hidden";
        }
+
+
 
        function showLiquid() {
            const button4 = document.getElementById("gas");
@@ -50,7 +62,19 @@
                 div.style.color = "gray";
              }
             });
+            const element = document.getElementById("tempSlider");
+  element.style.visibility = "hidden";
+  const element2 = document.getElementById("temperature");
+  element2.style.visibility = "hidden";
+  const element3 = document.getElementById("buttonsolid");
+  element3.style.visibility = "hidden";
+  const element4 = document.getElementById("buttonliquid");
+  element4.style.visibility = "hidden";
+  const element5 = document.getElementById("buttongas");
+  element5.style.visibility = "hidden";
        }
+
+
 
        function showSolid() {
            const button12 = document.getElementById("gas");
@@ -77,25 +101,19 @@
                 div.style.color = "gray";
              }
             });
+            const element = document.getElementById("tempSlider");
+            element.style.visibility = "hidden";
+            const element2 = document.getElementById("temperature");
+            element2.style.visibility = "hidden";
+            const element3 = document.getElementById("buttonsolid");
+            element3.style.visibility = "hidden";
+            const element4 = document.getElementById("buttonliquid");
+            element4.style.visibility = "hidden";
+            const element5 = document.getElementById("buttongas");
+            element5.style.visibility = "hidden";
        }
 
-       function showAll() {
-           const button8 = document.getElementById("gas");
-           button8.style.backgroundColor = "lightgray";
-           const button9 = document.getElementById("liquid");
-           button9.style.backgroundColor = "lightgray";
-           const button10 = document.getElementById("all");
-           button10.style.backgroundColor = "black";
-           const button11 = document.getElementById("solid");
-           button11.style.backgroundColor = "lightgrey";
-           const button12 = document.getElementById("nature");
-           button12.style.backgroundColor = "lightgrey";
-           const gridContainer = document.querySelector('.grid-container');
-           const divs = gridContainer.querySelectorAll('div');
-           divs.forEach(div => {
-            div.style = "default";
-           });
-       }
+       
 
        function showNature() {
         const button12 = document.getElementById("gas");
@@ -126,4 +144,68 @@
             div.style.color = "gray";
          }
          });
+         const element = document.getElementById("tempSlider");
+         element.style.visibility = "hidden";
+         const element2 = document.getElementById("temperature");
+         element2.style.visibility = "hidden";
+         const element3 = document.getElementById("buttonsolid");
+         element3.style.visibility = "hidden";
+         const element4 = document.getElementById("buttonliquid");
+         element4.style.visibility = "hidden";
+         const element5 = document.getElementById("buttongas");
+         element5.style.visibility = "hidden";
     }
+
+
+    const slider = document.getElementById("tempSlider");
+const output = document.getElementById("temperature");
+output.innerHTML = "Temperature: " + slider.value + "K"; // Display the default value
+
+slider.oninput = function() {
+  output.innerHTML = "Temperature: " + this.value + "K";
+}
+
+
+
+function showAll(temp) {
+   const button8 = document.getElementById("gas");
+   button8.style.backgroundColor = "lightgray";
+   const button9 = document.getElementById("liquid");
+   button9.style.backgroundColor = "lightgray";
+   const button10 = document.getElementById("all");
+   button10.style.backgroundColor = "black";
+   const button11 = document.getElementById("solid");
+   button11.style.backgroundColor = "lightgrey";
+   const button12 = document.getElementById("nature");
+   button12.style.backgroundColor = "lightgrey";
+   const gridContainer = document.querySelector('.grid-container');
+   const divs = gridContainer.querySelectorAll('div');
+   divs.forEach(div => {
+       if (div.id>0 && div.getAttribute('melt') <= parseInt(temp) && div.getAttribute('boil') > parseInt(temp)) {
+         div.style.backgroundColor = "orange";
+         div.style.color = "white";
+      }  
+        else if  (div.id>0 && div.getAttribute('melt') > parseInt(temp) ) {
+            div.style.backgroundColor = "black";
+            div.style.color = "white";
+         }
+         else if  (div.id>0 && div.getAttribute('boil') < parseInt(temp) ) {
+            div.style.backgroundColor = "red";
+            div.style.color = "white";
+         }
+
+
+         else div.style = "default";
+
+   })
+   const element = document.getElementById("tempSlider");
+element.style.visibility = "visible";
+const element2 = document.getElementById("temperature");
+element2.style.visibility = "visible";
+const element3 = document.getElementById("buttongas");
+element3.style.visibility = "visible";
+const element4 = document.getElementById("buttonliquid");
+element4.style.visibility = "visible";
+const element5 = document.getElementById("buttonsolid");
+element5.style.visibility = "visible";
+}
